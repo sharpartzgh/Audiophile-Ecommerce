14bac85ce2e3930d5baad640233751356add6b19
Frederick <120729769+sharpartzgh@users.noreply.github.com>
2024-01-31 15:34:22 +0000

    Setup for development done

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      "Manrope": ["Manrope", 'sans-serif']
+    },
+     screens: {
+
+      'mobile': {'min': '234px', 'max': '640px'},
+      //  => @media (min-width: 234px) and (max-width: 640px) 
+
+      'tablet': '640px',
+      // => @media (min-width: 640px) 
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) 
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) 
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
