7445b9a083426daa9b9acb9f56bc8e2209ecb052
sharpartzgh <sharpartzprojects@yahoo.com>
2024-02-08 13:10:16 +0000

    headphone page added

--- src/Pages/Headphones.jsx
+++ src/Pages/Headphones.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import HeadphoneDetail from '../components/Categories/Headphones/HeadphoneDetail'
+import Footer from '../components/Footer/Footer'
+
 
 const Headphones = () => {
+
+
   return (
-    <div>
+    <>
+      <div className="container__wrapper font-Manrope text-white bg-black text-center py-6">
+        <header className="leading-[44px] tablet:text-[40px] mobile:text-[28px] tracking-[1.43px]">HEADPHONES</header>
+      </div>
+      <div className="component__wrapper px-10">
+        <HeadphoneDetail/>
+      </div>
+      <div className="footer">
+        <Footer/>
+      </div>
       
-    </div>
+    </>
   )
 }
 
