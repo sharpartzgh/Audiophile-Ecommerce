4bdf094eb28f7d0ad99a8da2713d8e5c4432a147
sharpartzgh <sharpartzprojects@yahoo.com>
2024-02-08 13:16:10 +0000

    Speaker page added

--- src/Pages/Speakers.jsx
+++ src/Pages/Speakers.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Footer from '../components/Footer/Footer'
+import SpeakersDetail from '../components/Categories/Speaker/SpeakersDetail'
 
 const Speakers = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="container__wrapper font-Manrope text-white bg-black text-center py-6">
+        <header className="leading-[44px] tablet:text-[40px] mobile:text-[28px] tracking-[1.43px]">SPEAKERS</header>
+      </div>
+      <div className="component__wrapper px-10">
+        <SpeakersDetail/>
+      </div>
+      <div className="footer">
+        <Footer/>
+      </div>
+    </>
   )
 }
 
