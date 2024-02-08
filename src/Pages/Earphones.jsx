7933cf4f22f282f324c6c538dfbf7f694c4d9d36
sharpartzgh <sharpartzprojects@yahoo.com>
2024-02-08 13:13:17 +0000

    Earphone page added

--- src/Pages/Earphones.jsx
+++ src/Pages/Earphones.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Footer from '../components/Footer/Footer'
+
 
 const Earphones = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+        <div className="container__wrapper font-Manrope text-white bg-black text-center py-6">
+        <header className="leading-[44px] tablet:text-[40px] mobile:text-[28px] tracking-[1.43px]">EARPHONES</header>
+      </div>
+      <div className="component__wrapper px-10">
+     
+      </div>
+      <div className="footer">
+        <Footer/>
+      </div>
+    </>
   )
 }
 
